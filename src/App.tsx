722986debca81bd1a80d796623c9377f8fdf2e1f
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-09-04 15:13:14 +0700

    fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 
+
 import LandingPage from './Pages/LandingPage'
 import Login from "./Pages/Login"
 import Dashboard from "./Pages/Dashboard"
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import Navbar from '../../Components/Navbar';
-import Sidebar from '../../Components/Sidebar';
-import Footer from '../../Components/Footer';
-
+import Navbar from '../Components/Navbar';
+import Sidebar from '../Components/Sidebar';
+import Footer from '../Components/Footer';
 
 const Dashboard = () => {
     return (
