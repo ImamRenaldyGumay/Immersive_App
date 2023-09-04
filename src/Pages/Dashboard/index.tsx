92e57f41d23258b034de49755fbd757c616d0dcc
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-09-04 12:34:26 +0700

    fixed folder

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Navbar from '../../Components/Navbar';
+import Sidebar from '../../Components/Sidebar';
+import Footer from '../../Components/Footer';
+
+
+const Dashboard = () => {
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <main className="flex-grow">
+                    <Navbar />
+                    {/* Konten Utama Dashboard */}
+                </main>
+            </div>
+        </>
+    );
+}
+
+export default Dashboard
