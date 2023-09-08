fda7f7610eb823b7312beec57923f79a62e5d1f5
Xpow22 <diskaesfandiary22@gmail.com>
2023-09-08 16:39:44 +0700

    memperbaiki eror pada delete

--- src/Pages/ListUser/Adduser.tsx
+++ src/Pages/ListUser/Adduser.tsx
@@ -1,83 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Navbar from '../../Components/Navbar';
-import Sidebar from '../../Components/Sidebar';
+import Cookies from "js-cookie"
+import Navbar from "../../Components/Navbar";
+import Sidebar from "../../Components/Sidebar";
+import Swal from "sweetalert2";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Adduser = () => {
-    return (
-        <>
-            <div className="flex">
-                <Sidebar />
-                <main className="flex-grow">
-                    <Navbar />
-                    <div className='content-header flex justify-between m-4'>
-                        <h1 className='text-3xl font-bold leading-none tracking-tight'>Dashboard</h1>
-                    </div>
-                    <div className="max-w-full p-4 bg-white border border-gray-200 m-4 rounded-lg shadow">
-                        <h1 className='text-blue-dark text-center text-2xl'>NEW USER</h1>
-                        
-                        <div className="mb-4">
-                            <label htmlFor="fullname" className="block text-gray-700 font-semibold">Fullname</label>
-                            <input
-                                type="text"
-                                id="fullname"
-                                placeholder="Enter your fullname"
-                                className="w-full border border-gray-300 p-2 rounded-md"
-                            />
-                        </div>
-                        
-                        <div className="mb-4">
-                            <label htmlFor="email" className="block text-gray-700 font-semibold">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                placeholder="Enter your email"
-                                className="w-full border border-gray-300 p-2 rounded-md"
-                            />
-                        </div>
+    const navigate = useNavigate()
+    const token = Cookies.get("token");
+    const [FullName, setFullName] = useState("");
+    const [Email, setEmail] = useState("");
 
-                        <div className="mb-4 flex flex-row items-center justify-center">
-                            <div className="mr-4">
-                                <label htmlFor="team" className="block text-gray-700 font-semibold">Team</label>
-                                <select
-                                    id="team"
-                                    className="border border-gray-300 p-2 rounded-md"
-                                >
-                                    <option value="">Select Team</option>
-                                    <option value="team1">Team 1</option>
-                                    <option value="team2">Team 2</option>
-                                </select>
+    const handleFullName = (e: any) => {
+        setFullName(e?.target?.value);
+    }
+
+    const handleEmail = (e: any) => {
+        setEmail(e?.target?.value);
+    }
+
+    const submitUser = () => {
+        axios.post('users', {
+            "FullName": FullName, 
+            "Email" : Email
+            
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            console.log(response);
+
+            Swal.fire({
+                icon: "success",
+                title: "Success",
+                text: response.data.message,
+                confirmButtonText: "OK",
+            }).then(() => {
+                navigate("/Class")
+            });
+        }).catch(error => {
+            Swal.fire({
+                icon: "error",
+                title: "Failed",
+                text: `Something went wrong : ${error}`,
+                confirmButtonText: "OK",
+            });
+        })
+    }
+    
+        return (
+            <>
+                <div className="flex">
+                    <Sidebar />
+                    <main className="flex-grow">
+                        <Navbar />
+                        <div className='content-header flex justify-between m-4'>
+                            <h1 className='text-3xl font-bold leading-none tracking-tight'>Dashboard</h1>
+                        </div>
+                        <div className="max-w-full p-4 bg-white border border-gray-200 m-4 rounded-lg shadow">
+                            <h1 className='text-blue-dark text-center text-2xl'>NEW USER</h1>
+
+                            <div className="mb-4">
+                                <label htmlFor="fullname" className="block text-gray-700 font-semibold">Fullname</label>
+                                <input
+                                    type="text"
+                                    id="fullname"
+                                    value={FullName}
+                                    placeholder="Enter your fullname"
+                                    className="w-full border border-gray-300 p-2 rounded-md"
+                                    onChange={handleFullName}
+                                />
                             </div>
 
-                            <div className="mr-4">
-                                <label htmlFor="role" className="block text-gray-700 font-semibold">Role</label>
-                                <select
-                                    id="role"
-                                    className="border border-gray-300 p-2 rounded-md"
-                                >
-                                    <option value="">Select Role</option>
-                                    <option value="admin">Admin</option>
-                                    <option value="user">User</option>
-                                </select>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="block text-gray-700 font-semibold">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    value={Email}
+                                    placeholder="Enter your email"
+                                    className="w-full border border-gray-300 p-2 rounded-md"
+                                    onChange={handleEmail}
+                                />
                             </div>
 
-                            <div>
-                                <label htmlFor="status" className="block text-gray-700 font-semibold">Status</label>
-                                <select
-                                    id="status"
-                                    className="border border-gray-300 p-2 rounded-md"
+                            <div className="mb-4 flex flex-row items-center justify-center">
+                                <div className="mr-4">
+                                    <label htmlFor="team" className="block text-gray-700 font-semibold">Team</label>
+                                    <select
+                                        id="team"
+                                        className="border border-gray-300 p-2 rounded-md"
+                                    >
+                                        <option value="">Select Team</option>
+                                        <option value="team1">Front-End</option>
+                                        <option value="team2">Back-End</option>
+                                        <option value="team2">Quality Assurance</option>
+                                    </select>
+                                </div>
+
+                                <div className="mr-4">
+                                    <label htmlFor="role" className="block text-gray-700 font-semibold">Role</label>
+                                    <select
+                                        id="role"
+                                        className="border border-gray-300 p-2 rounded-md"
+                                    >
+                                        <option value="">Select Role</option>
+                                        <option value="admin">Admin</option>
+                                        <option value="user">User</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <label htmlFor="status" className="block text-gray-700 font-semibold">Status</label>
+                                    <select
+                                        id="status"
+                                        className="border border-gray-300 p-2 rounded-md"
+                                    >
+                                        <option value="">Select Status</option>
+                                        <option value="active">Active</option>
+                                        <option value="inactive">No-Active</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="mt-4 text-center">
+                                <button
+                                    className="mt-12 px-10 py-2 bg-blue-dark text-white rounded-md shadow-lg text-xl focus:outline-none tracking-wide"
+                                    onClick={submitUser}
                                 >
-                                    <option value="">Select Status</option>
-                                    <option value="active">Active</option>
-                                    <option value="inactive">No-Active</option>
-                                </select>
+                                    SAVE
+                                </button>
                             </div>
                         </div>
-                    </div>
-                </main>
-            </div>
-        </>
-    );
-}
+                    </main>
+                </div>
+            </>
+        );
+    }
 
-export default Adduser;
+    export default Adduser;
